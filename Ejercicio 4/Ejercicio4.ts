b4124ebe9153fda073769b5b3b55b4244b45f051
AgusRodrigues <agusrodrigues199@gmail.com>
2024-04-03 17:49:31 -0300

    mas ejercicios

---
+++ Ejercicio 4/Ejercicio4.ts
@@ -0,0 +1,60 @@
+/* 
+
+interface Task {
+    title: string;
+    prioridad: 'Alta' | 'Media' | 'Baja';
+    completada: boolean;
+    persona: string;
+  }
+
+const tareas = [
+  { title: 'Tarea 1', prioridad: 'High', completada: false, persona: 'Alice' },
+  { title: 'Tarea 2', prioridad: 'Medium', completada: true, persona: 'Bob' },
+  { title: 'Tarea 3', prioridad: 'Low', completada: false, persona: 'Alice' },
+  { title: 'Tarea 4', prioridad: 'High', completada: false, persona: 'Charlie' },
+  { title: 'Tarea 5', prioridad: 'Medium', completada: true, persona: 'Bob' },
+];
+
+function isValidName(name: string): boolean {
+  // Regular expression to check if the name is valid
+  const nameRegex = /^[a-zA-ZÀ-ÿ]+([ '-][a-zA-ZÀ-ÿ]+)?$/;
+
+  // Test the name against the regular expression
+  return nameRegex.test(name);
+}
+
+function getTasks() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(tareas);
+    }, 3000);
+  });
+}
+
+async function displayTasks(persona) {
+  try {
+    const tasksList = await getTasks();
+    const filteredTasks = tasksList.filter((tareas) => tarea.persona === persona);
+
+    console.log(`Tareas assigned to ${persona}:`);
+
+    filteredTasks
+      .filter((tareas) => !tareas.completed)
+      .sort((a, b) => {
+        if (a.prioridad < b.prioridad) return -1;
+        if (a.prioridad > b.prioridad) return 1;
+        return 0;
+      })
+      .forEach((tarea) => console.log(`- ${tarea.title} (prioridad: ${tarea.prioridad})`));
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+displayTasks('Alice');
+
+
+
+
+*/
