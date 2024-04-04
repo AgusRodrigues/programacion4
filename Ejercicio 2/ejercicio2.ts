0a255f6edfe2711ee28a2496f6f7f7c6af52fe6c
AgusRodrigues <agusrodrigues199@gmail.com>
2024-04-04 12:59:25 -0300

    ejercicio 2 correcciones

--- Ejercicio 2/ejercicio2.ts
+++ Ejercicio 2/ejercicio2.ts
@@ -1,33 +1,35 @@
 interface Task {
-    title: string;
-    prioridad: 'Alta' | 'Media' | 'Baja';
-    completada: boolean;
-  }
-  
-  const tasks: Task[] = [
-    { title: 'Task 1', prioridad: 'Alta', completada: false },
-    { title: 'Task 2', prioridad: 'Media', completada: true },
-    { title: 'Task 3', prioridad: 'Baja', completada: false },
-    { title: 'Task 4', prioridad: 'Alta', completada: false }
-  ];
-  
-  function getTasks(): Promise<Task[]> {
-    return new Promise<Task[]>((resolve) => {
-      setTimeout(() => {
-        resolve(tasks);
-      }, 3000);
-    });
-  }
-  
-  getTasks().then((tasks) => {
-    console.log('Pending tasks:');
-  
-    tasks
-      .filter((task) => !task.completada)
-      .sort((a, b) => {
-        if (a.prioridad < b.prioridad) return -1;
-        if (a.prioridad > b.prioridad) return 1;
-        return 0;
-      })
-      .forEach((task) => console.log(`- ${task.title} (priority: ${task.prioridad})`));
+  title: string;
+  prioridad: "Alta" | "Media" | "Baja";
+  completada: boolean;
+}
+
+const tasks: Task[] = [
+  { title: "Task 1", prioridad: "Alta", completada: false },
+  { title: "Task 2", prioridad: "Media", completada: true },
+  { title: "Task 3", prioridad: "Baja", completada: false },
+  { title: "Task 4", prioridad: "Alta", completada: false },
+];
+
+function getTasks(): Promise<Task[]> {
+  return new Promise<Task[]>((resolve) => {
+    setTimeout(() => {
+      resolve(tasks);
+    }, 3000);
   });
+}
+
+getTasks().then((tasks) => {
+  console.log("Pending tasks:");
+
+  tasks
+    .filter((task) => !task.completada)
+    .sort((a, b) => {
+      if (a.prioridad < b.prioridad) return -1;
+      if (a.prioridad > b.prioridad) return 1;
+      return 0;
+    })
+    .forEach((task) =>
+      console.log(`- ${task.title} (priority: ${task.prioridad})`)
+    );
+});
